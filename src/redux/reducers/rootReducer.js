f01940b412c87e3c7a4c3f80ebeba10e5cd6f071
Brande Gilbert <brandegilbert@gmail.com>
2021-10-26 16:05:19 -0400

    Update

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,9 +1,8 @@
 import { combineReducers } from "redux";
-import CarData from "../reducers/CarData";
+
 import CartReducer from "./CartReducer";
 
 const rootReducer = combineReducers({
-  // CarData,
   CartReducer,
 });
 
