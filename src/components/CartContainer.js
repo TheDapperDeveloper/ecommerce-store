09ef71343de3f1790f1ca181f9b7585c9f084271
Brande Gilbert <brandegilbert@gmail.com>
2021-10-26 16:34:56 -0400

    Update

---
+++ src/components/CartContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { CardContainer } from "../styled-components/InventoryCardContainerStyle";
+
+export default function CartContainer() {
+  return (
+    <div>
+      <CardContainer></CardContainer>
+    </div>
+  );
+}
