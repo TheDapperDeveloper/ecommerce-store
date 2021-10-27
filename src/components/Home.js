c0753c7876fa81e54d43c2426017eb284fffddde
Brande Gilbert <brandegilbert@gmail.com>
2021-10-27 12:13:17 -0400

    Home page styling slideshow update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 import Inventory from "./Inventory";
+import Slideshow from "./Slideshow";
 
 export default function Home(props) {
   console.log(props.carInfo);
+
+  const images = props.carInfo;
   return (
     <div>
-      <h1>Garage</h1>
-      {props.carInfo.map((car) => (
+      <Slideshow images={images} />
+      {/* {props.carInfo.map((car) => (
         <img src={car.Image} alt=""></img>
-      ))}
+      ))} */}
     </div>
   );
 }
